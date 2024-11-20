01b102824dd3bd210e9b962750fa89e7358cc9d6
Hamid Peywasti <hamid@respinar.com>
2024-11-20 13:31:00 +0330

    rename bundle to Respinar/ContaoRedirectionBundle

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -3,27 +3,27 @@
 declare(strict_types=1);
 
 /*
- * This file is part of Contao Redirect Bundle.
+ * This file is part of Contao Redirection Bundle.
  *
  * (c) Hamid Peywasti 2025 <hamid@respinar.com>
  *
  * @license MIT
  */
 
-namespace Respinar\ContaoRedirectBundle\ContaoManager;
+namespace Respinar\ContaoRedirectionBundle\ContaoManager;
 
 use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
-use Respinar\ContaoRedirectBundle\RespinarContaoRedirectBundle;
+use Respinar\ContaoRedirectionBundle\RespinarContaoRedirectionBundle;
 
 class Plugin implements BundlePluginInterface
 {
     public function getBundles(ParserInterface $parser): array
     {
         return [
-            BundleConfig::create(RespinarContaoRedirectBundle::class)
+            BundleConfig::create(RespinarContaoRedirectionBundle::class)
                 ->setLoadAfter([ContaoCoreBundle::class]),
         ];
     }
