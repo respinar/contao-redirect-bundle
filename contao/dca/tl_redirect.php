1accd3393dc0e5531715047615c7008736ab6835
Hamid Peywasti <hamid@respinar.com>
2024-12-18 12:20:47 +0330

    add tl_redirect to dca

---
+++ contao/dca/tl_redirect.php
@@ -0,0 +1,71 @@
+<?php
+declare(strict_types=1);
+// src/Resources/contao/dca/tl_redirect.php
+
+use Contao\DC_Table;
+
+$GLOBALS['TL_DCA']['tl_redirect'] = [
+    'config' => [
+        'dataContainer' => DC_Table::class,
+        'enableVersioning' => true,
+        'sql' => [
+            'keys' => [
+                'id' => 'primary',
+            ],
+        ],
+    ],
+    'list' => [
+        'sorting' => [
+            'mode' => 1,
+            'fields' => ['source_url'],
+        ],
+        'label' => [
+            'fields' => ['source_url', 'target_url'],
+            'format' => '%s → %s',
+        ],
+        'operations' => [
+            'edit' => ['href' => 'act=edit', 'icon' => 'edit.svg'],
+            'delete' => ['href' => 'act=delete', 'icon' => 'delete.svg'],
+        ],
+    ],
+    'palettes' => [
+        'default' => '{redirect_legend},source_url,target_url;{settings_legend},status_code,active',
+    ],
+    'fields' => [
+        'id' => [
+            'sql' => "int(10) unsigned NOT NULL auto_increment",
+        ],
+        'tstamp' => [
+            'sql' => "int(10) unsigned NOT NULL default '0'",
+        ],
+        'source_url' => [
+            'label' => ['Source URL', 'The URL to redirect from'],
+            'inputType' => 'text',
+            'eval' => ['mandatory' => true, 'tl_class' => 'w50'],
+            'sql' => "varchar(255) NOT NULL default ''",
+        ],
+        'target_url' => [
+            'label' => ['Target URL', 'The URL to redirect to (ignored for 410)'],
+            'inputType' => 'text',
+            'eval' => ['mandatory' => true, 'tl_class' => 'w50'],
+            'sql' => "varchar(255) NOT NULL default ''",
+        ],
+        'status_code' => [
+            'label' => ['HTTP Status', 'The redirect status code'],
+            'inputType' => 'select',
+            'options' => [
+                '301' => '301 Permanent Redirect',
+                '302' => '302 Temporary Redirect',
+                '410' => '410 Gone', // Added 410 option
+            ],
+            'eval' => ['tl_class' => 'w50'],
+            'sql' => "varchar(3) NOT NULL default '301'",
+        ],
+        'active' => [
+            'label' => ['Active', 'Enable this redirect'],
+            'inputType' => 'checkbox',
+            'eval' => ['tl_class' => 'w50'],
+            'sql' => "char(1) NOT NULL default '1'",
+        ],
+    ],
+];
